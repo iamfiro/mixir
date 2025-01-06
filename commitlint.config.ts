00213bc34adf70dda8d79e67fc5ead117197d12a
iamfiro <appturbo102@gmail.com>
2025-01-06 21:06:40 +0900

    chore: 커밋 전 lint husky 세팅 #1

---
+++ commitlint.config.ts
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+    ],
+    "scope-enum": [2, "always", ["ui", "backend", "build", "docs", "deps"]],
+    "header-max-length": [2, "always", 72],
+    'references-empty': [2, 'never'], // Ensures there is at least one issue reference
+    'header-pattern': [
+      2,
+      'always',
+      /^.*\s#\d+$/ // Ensures the message ends with an issue number like #123
+    ],
+  },
+};

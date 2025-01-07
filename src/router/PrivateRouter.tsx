8eabed7ba47ac96b6b6646eca586be1ab5c8b012
iamfiro <appturbo102@gmail.com>
2025-01-07 21:16:36 +0900

    feat: add PrivateRoute component to protect routes based on authentication status #3

---
+++ src/router/PrivateRouter.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from "react-router";
+import { checkAuthenticate } from "../utils/api";
+
+const PrivateRoute = () => {
+    const isAuthenticated = checkAuthenticate();
+
+    if (!isAuthenticated) {
+        alert('로그인 후 이용이 가능합니다')
+    }
+
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+}
+
+export default PrivateRoute;

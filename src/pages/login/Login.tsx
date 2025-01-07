94cc16ebf91ea5c9d6156b13ebd51442b1aa7db4
iamfiro <appturbo102@gmail.com>
2025-01-07 21:05:40 +0900

    feat: update Login page to include Google login button with icon #2

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,7 @@
-import { Button, Viewport } from "../../components/common"
-import { Container, Description, Title } from "./styles.css"
+import { Button, Viewport } from '../../components/common'
+import { Container, Description, Title } from './styles.css'
 import MixirLogo from '../../assets/logo.tsx'
+import GoogleLogo from '../../assets/icon/GoogleLoginIcon.tsx'
 
 const PageLogin = () => {
     return (
@@ -8,13 +9,19 @@ const PageLogin = () => {
             <Container>
                 <MixirLogo size={60} color="#fff" />
                 <Title>팀 관리를 Mixir앱 하나로</Title>
-                <Description>
-                    설명을 적어주세요
-                </Description>
-                <Button variant="background" size="lg" isLoading>로그인</Button>
+                <Description>설명을 적어주세요</Description>
+                <Button
+                    variant="background"
+                    size="lg"
+                    fullWidth
+                    style={{ marginTop: 20 }}
+                    leadingIcon={<GoogleLogo size={24} />}
+                >
+                    눌러서 Google로 시작하기
+                </Button>
             </Container>
         </Viewport>
     )
 }
 
-export default PageLogin;
+export default PageLogin

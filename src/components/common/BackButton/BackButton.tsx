f330b8ee75b2a9a4b2566a3df77fc046004a7506
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-06 08:55:55 +0900

    feat: add back navigation functionality to BackButton component

--- src/components/common/BackButton/BackButton.tsx
+++ src/components/common/BackButton/BackButton.tsx
@@ -1,10 +1,17 @@
 import { IoIosArrowBack } from "react-icons/io";
 import { BackButtonContainerStyle } from './BackButton.style.ts'
+import { useNavigate } from "react-router";
 
 const BackButton = () => {
+    const navigate = useNavigate();
+
+    const handleBackButtonClick = () => {
+        navigate(-1);
+    }
+
     return (
         <BackButtonContainerStyle>
-            <IoIosArrowBack size={26} color={'var(--color-secondary-light)'} />
+            <IoIosArrowBack size={26} color={'var(--color-secondary-light)'} onClick={handleBackButtonClick} />
         </BackButtonContainerStyle>
     )
 }
